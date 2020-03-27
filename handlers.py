ded858afab36068450a7e382d11833081a18b99e
quiet-cmd <89250609951@yandex.ru>
2020-03-27 13:48:41 +0300

    bot logic

--- handlers.py
+++ handlers.py
@@ -1,13 +1,24 @@
 from main import bot, dp
 from aiogram.types import Message
 from config import admin_id
-import apiai, json
-from config import TOKEN_DF, NAME_BOT
+import apiai
+import json
+from config import TOKEN_DF, NAME_BOT, database_name
+from SQLighter import *
+from keyboards import ListOfButtons
 
 
-# При запуске бота админ получит сообщение.
 async def send_to_admin(*args):
     await bot.send_message(chat_id=admin_id, text="Фростморн жаждет крови!")
+
+
+# кнопки - название таблиц бд
+@dp.message_handler(commands='test')
+async def keyboards(message: Message):
+    text = "Выберите тест"
+    name = SQLighter(database_name).all_table_name()
+    keyboard = ListOfButtons(text=name, callback=list(range(len(name)))).inline_keyboard
+    await message.answer(text=text, reply_markup=keyboard)
 
 
 # наш DialogFlow
@@ -19,4 +30,7 @@ async def echo(message: Message):
     request.query = message.text
     response_json = json.loads(request.getresponse().read().decode("utf-8"))
     response = response_json["result"]["fulfillment"]["speech"]
-    await message.answer(text=response)
+    if response:
+        await message.answer(text=response)
+    else:
+        await message.answer(text="Я не знаю что вам ответить")
