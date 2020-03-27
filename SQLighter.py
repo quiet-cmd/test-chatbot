1c111ff54fa163aff1709b959f3e603e9d66dc8c
quiet-cmd <89250609951@yandex.ru>
2020-03-27 13:33:21 +0300

    sql queries

--- SQLighter.py
+++ SQLighter.py
@@ -11,22 +11,22 @@ class SQLighter:
     def all_table_name(self):
         """ Получаем все имена таблиц """
         with self.connection:
-            return self.cursor.execute('SELECT name from sqlite_master where type= "table"').fetchall()
+            return [i[0] for i in self.cursor.execute('SELECT name from sqlite_master where type= "table"').fetchall()]
 
     def select_all(self, table_name):
         """ Получаем все строки """
         with self.connection:
-            return self.cursor.execute('SELECT * FROM  {0}'.format(table_name[0])).fetchall()
+            return self.cursor.execute('SELECT * FROM  {0}'.format(table_name)).fetchall()
 
     def select_single(self, table_name, rownum):
         """ Получаем одну строку с номером rownum """
         with self.connection:
-            return self.cursor.execute('SELECT * FROM {0} WHERE id = ?'.format(table_name[0]), (rownum,)).fetchall()[0]
+            return self.cursor.execute('SELECT * FROM {0} WHERE id = ?'.format(table_name), (rownum,)).fetchall()[0]
 
     def count_rows(self, table_name):
         """ Считаем количество строк """
         with self.connection:
-            result = self.cursor.execute('SELECT * FROM {0}}'.format(table_name[0])).fetchall()
+            result = self.cursor.execute('SELECT * FROM {0}'.format(table_name)).fetchall()
             return len(result)
 
     def close(self):
