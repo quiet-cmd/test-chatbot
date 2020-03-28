50ec1dfa313fadc1784673c2c1bae694c0155da4
quiet-cmd <89250609951@yandex.ru>
2020-03-28 19:45:57 +0300

    bot logic

--- handlers.py
+++ handlers.py
@@ -1,20 +1,31 @@
 from main import bot, dp
-from aiogram.types import Message
 import apiai
 import json
 from config import TOKEN_DF, NAME_BOT, database_name
 from SQLighter import SQLighter
-from aiogram.types import InlineKeyboardMarkup, InlineKeyboardButton, ReplyKeyboardMarkup, KeyboardButton
+from keyboards import ListOfButtons
+from filters import *
 
 
 # кнопки - название таблиц бд
-@dp.message_handler(commands='start')
-async def start_cmd_handler(message: Message):
-    keyboard_markup = InlineKeyboardMarkup(row_width=2)
+@dp.message_handler(commands='test')
+async def keyboards_start(message: Message):
     text = SQLighter(database_name).all_table_name()
-    data = list(range(len(text)))
-    keyboard_markup.row(*(InlineKeyboardButton(text, callback_data=data) for text, data in zip(text, data)))
-    await message.reply("Выберите тест", reply_markup=keyboard_markup)
+    data = SQLighter(database_name).all_table_name()
+    keyboard = ListOfButtons(text=text, callback=data).inline_keyboard
+    await message.answer(text="Выберите тест", reply_markup=keyboard)
+
+
+@dp.callback_query_handler(Button("Физкультура1"))
+async def с_btn1(call: CallbackQuery):
+    db_work = SQLighter(database_name)
+    table_name = "Физкультура1"
+    for i in range(1, db_work.count_rows(table_name) + 1):
+        right_answer = db_work.select_right_answer(table_name, i)[0]
+        answers = str(db_work.select_wrong_answers(table_name, i)[0]).split(';')
+        answers.append(right_answer)
+        keyboard = ListOfButtons(text=answers, callback=answers).inline_keyboard
+        await call.message.answer(text=str(db_work.select_question(table_name, i)[0]), reply_markup=keyboard)
 
 
 # наш DialogFlow
@@ -26,7 +37,4 @@ async def echo(message: Message):
     request.query = message.text
     response_json = json.loads(request.getresponse().read().decode("utf-8"))
     response = response_json["result"]["fulfillment"]["speech"]
-    if response:
-        await message.answer(text=response)
-    else:
-        await message.answer(text="Я не знаю что вам ответить")
+    await message.answer(text=response)
