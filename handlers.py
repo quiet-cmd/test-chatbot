5a27fa7a993a7413a095869339dcd43a8ae41a3d
quiet-cmd <89250609951@yandex.ru>
2020-04-04 15:07:45 +0300

    bot logic

--- handlers.py
+++ handlers.py
@@ -1,31 +1,37 @@
-from main import bot, dp
+from main import bot, dp, types
 import apiai
 import json
 from config import TOKEN_DF, NAME_BOT, database_name
 from SQLighter import SQLighter
-from keyboards import ListOfButtons
-from filters import *
+import random
+from aiogram.types import Message, CallbackQuery
 
 
 # кнопки - название таблиц бд
 @dp.message_handler(commands='test')
 async def keyboards_start(message: Message):
+    keyboard_test_name = types.InlineKeyboardMarkup()
     data = SQLighter(database_name).all_table_name()
     text = [i.replace('_', ' ') for i in data]
-    keyboard = ListOfButtons(text=text, callback=data).inline_keyboard
-    await message.answer(text="Выберите тест", reply_markup=keyboard)
+    text_and_data = zip(text, data)
+    row_btn = (types.InlineKeyboardButton(text, callback_data=data) for text, data in text_and_data)
+    keyboard_test_name.row(*row_btn)
+    await message.answer(text="Выберите тест", reply_markup=keyboard_test_name)
 
 
-@dp.callback_query_handler(Button("Физкультура_1"))
+@dp.callback_query_handler(text=["Физкультура_1", "Физкультура_2"])
 async def keyboards_test(call: CallbackQuery):
     db_work = SQLighter(database_name)
     table_name = str(call.data)
-    print(table_name)
     for i in range(1, db_work.count_rows(table_name) + 1):
+        keyboard = types.InlineKeyboardMarkup()
         right_answer = db_work.select_right_answer(table_name, i)[0]
-        answers = str(db_work.select_wrong_answers(table_name, i)[0]).split(';')
-        answers.append(right_answer)
-        keyboard = ListOfButtons(text=answers, callback=answers).inline_keyboard
+        wrong_answer = str(db_work.select_wrong_answers(table_name, i)[0])
+        answer = wrong_answer.split(';')
+        answer.append(right_answer)
+        random.shuffle(answer)
+        row_btn = (types.InlineKeyboardButton(text, callback_data=text) for text in answer)
+        keyboard.row(*row_btn)
         await call.message.answer(text=str(db_work.select_question(table_name, i)[0]), reply_markup=keyboard)
 
 
