8d038431bcc50be2de8fd03071681da1bff30d78
quiet-cmd <89250609951@yandex.ru>
2020-04-07 12:02:35 +0300

    bot logic

--- main.py
+++ main.py
@@ -1,17 +1,21 @@
 import asyncio
-from aiogram import Bot, Dispatcher, executor, types
+from aiogram import Bot, Dispatcher, executor, types, md
 from aiogram.types import Message, CallbackQuery
 from aiogram.contrib.fsm_storage.memory import MemoryStorage
+from aiogram.utils.callback_data import CallbackData
 from config import TOKEN_DF, NAME_BOT, database_name, TOKEN_TG
 from SQLighter import SQLighter
 import random
 import json
 import apiai
 
+
 loop = asyncio.get_event_loop()
-bot = Bot(TOKEN_TG, parse_mode="HTML")
+bot = Bot(TOKEN_TG, parse_mode='HTML')
 storage = MemoryStorage()
 dp = Dispatcher(bot, storage=storage, loop=loop)
+
+posts_cb = CallbackData('post', 'data', 'action', 'index')
 
 
 @dp.message_handler(commands='start')
@@ -28,34 +32,62 @@ async def test_name(message: Message):
     data = SQLighter(database_name).all_table_name()
     text = [i.replace('_', ' ') for i in data]
     text_and_data = zip(text, data)
-    row_btn = (types.InlineKeyboardButton(text, callback_data=data) for text, data in text_and_data)
+    row_btn = (types.InlineKeyboardButton(text, callback_data=posts_cb.new(data=data, action='test', index=1)) for
+               text, data in
+               text_and_data)
     keyboard_test_name.row(*row_btn)
     await message.answer(text="Выберите тест", reply_markup=keyboard_test_name)
 
 
-@dp.callback_query_handler(text=["Физкультура_1", "Физкультура_2"])
-async def user_testing(call: CallbackQuery):
+@dp.callback_query_handler(posts_cb.filter(action=['test', 'step+', 'step-']))
+async def testing_users(query: types.CallbackQuery, callback_data: dict):
+    index = int(callback_data['index'])
     db_work = SQLighter(database_name)
-    table_name = str(call.data)
-    for i in range(1, db_work.count_rows(table_name) + 1):
-        keyboard = types.InlineKeyboardMarkup()
-        right_answer = db_work.select_right_answer(table_name, i)[0]
-        answer = str(db_work.select_wrong_answers(table_name, i)[0]).split(';')
-        answer.append(right_answer)
-        random.shuffle(answer)
-        row_btn = (types.InlineKeyboardButton(text, callback_data=text) for text in answer)
-        keyboard.row(*row_btn)
-        await call.message.edit_text(text=str(db_work.select_question(table_name, i)[0]), reply_markup=keyboard)
+    table_name = callback_data['data']
+
+    if callback_data['action'] == 'step+':
+        if index < db_work.count_rows(table_name):
+            index += 1
+        else:
+            await query.answer(text='Тут будет результат теста')
+    if callback_data['action'] == 'step-':
+        if index > 1:
+            index -= 1
+        else:
+            await query.answer(text='У нас нет нулевого вопроса')
+
+    text = md.text(
+        md.hbold(f'Вопрос {index} из {db_work.count_rows(table_name)}'),
+        md.quote_html(db_work.select_question(table_name, index)),
+        '',
+        sep='\n')
+
+    right_answer = db_work.select_right_answer(table_name, index)
+
+    answer = db_work.select_wrong_answers(table_name, index).split(';')
+    answer.append(right_answer)
+    random.shuffle(answer)
+    answer = {i: '1' if i == right_answer else '0' for i in answer}
+
+    markup = types.InlineKeyboardMarkup()
+    row_btn = (types.InlineKeyboardButton(text=text, callback_data=posts_cb.new(data=text, action=b, index=index)) for
+               text, b in
+               answer.items())
+    markup.row(*row_btn)
+    markup.add(
+        types.InlineKeyboardButton('Next', callback_data=posts_cb.new(data=table_name, action='step+', index=index)),
+        types.InlineKeyboardButton('Back', callback_data=posts_cb.new(data=table_name, action='step-', index=index)))
+    await query.message.edit_text(text=text, reply_markup=markup)
 
 
 @dp.message_handler()
 async def message_reply(message: Message):
     request = apiai.ApiAI(TOKEN_DF).text_request()
-    request.lang = "ru"
+    request.lang = 'ru'
     request.session_id = NAME_BOT
     request.query = message.text
-    response_json = json.loads(request.getresponse().read().decode("utf-8"))
-    response = response_json["result"]["fulfillment"]["speech"]
+    response_json = json.loads(request.getresponse().read().decode('utf-8'))
+    response = response_json['result']['fulfillment']['speech']
     if response:
         await message.answer(text=response)
     else:
@@ -63,5 +95,4 @@ async def message_reply(message: Message):
 
 
 if __name__ == '__main__':
-
     executor.start_polling(dp)
