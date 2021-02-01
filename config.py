c6ad8861ba2e29e3b519849e4d840556a0f5d80b
quiet-cmd <89250609951@yandex.ru>
2021-02-01 16:50:51 +0300

    delete STORAGE_NAME

--- config.py
+++ config.py
@@ -3,4 +3,3 @@ TOKEN_TG = str(os.environ.get('telegram_token'))
 TOKEN_DF = str(os.environ.get('dialogflow_token'))
 NAME_BOT = str(os.environ.get('bot_name'))
 DATABASE_NAME = 'tests.db'
-STORAGE_NAME = 'storage.db'
